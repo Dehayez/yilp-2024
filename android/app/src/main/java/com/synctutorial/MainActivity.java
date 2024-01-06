34eea1623434aff833fea5626c9e189b26829260
Dehayez <yentel.dh@hotmail.be>
2024-01-06 16:44:17 +0100

    changed project's name

--- android/app/src/main/java/com/synctutorial/MainActivity.java
+++ android/app/src/main/java/com/synctutorial/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "SyncTutorial";
+    return "Yilp";
   }
 
   /**
--- ios/YilpTests/YilpTests.m
+++ ios/YilpTests/YilpTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SyncTutorialTests : XCTestCase
+@interface YilpTests : XCTestCase
 
 @end
 
-@implementation SyncTutorialTests
+@implementation YilpTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
